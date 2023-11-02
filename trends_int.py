d8c24801926afd12369f9cfff9f837ae1026b47e
eylulranasarac <100572861+eylulranasarac@users.noreply.github.com>
2023-11-02 13:02:11 +0300

    Add files via upload

---
+++ trends_int.py
@@ -0,0 +1,29 @@
+from pytrends.request import TrendReq
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# pytrends oturumunu başlat
+pytrend = TrendReq()
+
+# Google Trends için sorgu ayarları
+kw_list = ["PETKM"]  # Örnek anahtar kelime listesi
+timeframe = '2017-08-01 2017-09-01'  # Belirli bir tarih aralığı
+pytrend.build_payload(kw_list, timeframe=timeframe, geo='TR')
+
+# Verileri al
+trends_data = pytrend.interest_over_time()
+
+# Gereksiz sütunları sil (örneğin, 'isPartial')
+trends_data = trends_data.drop(labels=['isPartial'], axis='columns')
+
+# Veriyi kontrol et
+print(trends_data.head())
+
+# Grafik çiz
+plt.figure(figsize=(10, 5))
+plt.plot(trends_data.index, trends_data[kw_list[0]])
+plt.title('Monthly Search Amount of PETKM in Turkey')
+plt.xlabel('Date')
+plt.ylabel('PETKM Search')
+plt.tight_layout()  # Grafiğin düzgün görünmesi için
+plt.show()
