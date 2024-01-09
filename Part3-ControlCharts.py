c5e5e9fdc63fda400ca7c6fa305914ef70bd7333
mervenurhundur <146987717+mervenurhundur@users.noreply.github.com>
2024-01-09 19:22:21 +0300

    new commit

--- Part3-ControlCharts.py
+++ Part3-ControlCharts.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import cv2
 
 # Fotoğraf dosyasının adını ve yolunu belirtin
-image_path = "C:/Users/ASUS/Desktop/423-part3/0012.jpg"
+image_path = "C:/Users/ASUS/Desktop/423-part3/0076.jpg"
 
 # OpenCV ile resmi yükle
 # Grayscale olarak oku
@@ -44,45 +44,67 @@ def calculate_control_limits_per_column(data):
 
     return control_limits
 
-# Function to find out-of-control points for each row or column
-def find_out_of_control_points(data, control_limits):
+# Function to find out-of-control points for each row
+def find_out_of_control_points_per_row(data, control_limits):
     out_of_control_points = {}
 
-    for i, (row_or_col, limits) in enumerate(control_limits.items()):
+    for i, (row, limits) in enumerate(control_limits.items()):
         upper_limit = limits['Upper Limit']
         lower_limit = limits['Lower Limit']
 
-        # Find indices of points outside the control limits
+        # Find indices of points outside the control limits for rows
         out_of_control_indices = np.where((data[i, :] > upper_limit) | (data[i, :] < lower_limit))[0]
 
-        # Save the indices for each row or column
-        out_of_control_points[row_or_col] = out_of_control_indices
+        # Save the indices for each row
+        out_of_control_points[row] = out_of_control_indices
 
     return out_of_control_points
 
-# Function to set out-of-control points to black
-def set_out_of_control_points_to_black(data, out_of_control_points):
+# Function to find out-of-control points for each column
+def find_out_of_control_points_per_column(data, control_limits):
+    out_of_control_points = {}
+
+    for j, (col, limits) in enumerate(control_limits.items()):
+        upper_limit = limits['Upper Limit']
+        lower_limit = limits['Lower Limit']
+
+        # Find indices of points outside the control limits for columns
+        out_of_control_indices = np.where((data[:, j] > upper_limit) | (data[:, j] < lower_limit))[0]
+
+        # Save the indices for each column
+        out_of_control_points[col] = out_of_control_indices
+
+    return out_of_control_points
+
+# Function to set out-of-control points to black for rows
+def set_out_of_control_points_to_black_per_row(data, out_of_control_points):
     modified_data = np.copy(data)
 
-    for row_or_col, indices in out_of_control_points.items():
-        if "Row" in row_or_col:
-            row_index = int(row_or_col.split()[1]) - 1
-            modified_data[row_index, indices] = 0
-        elif "Column" in row_or_col:
-            col_index = int(row_or_col.split()[1]) - 1
-            modified_data[indices, col_index] = 0
+    for row, indices in out_of_control_points.items():
+        row_index = int(row.split()[1]) - 1
+        modified_data[row_index, indices] = 0
+
+    return modified_data
+
+# Function to set out-of-control points to black for columns
+def set_out_of_control_points_to_black_per_column(data, out_of_control_points):
+    modified_data = np.copy(data)
+
+    for col, indices in out_of_control_points.items():
+        col_index = int(col.split()[1]) - 1
+        modified_data[indices, col_index] = 0
 
     return modified_data
 
 # Call the function to get the modified data for rows
 control_limits_per_row = calculate_control_limits_per_row(original_data)
-out_of_control_points_per_row = find_out_of_control_points(original_data, control_limits_per_row)
-modified_data_per_row = set_out_of_control_points_to_black(original_data, out_of_control_points_per_row)
+out_of_control_points_per_row = find_out_of_control_points_per_row(original_data, control_limits_per_row)
+modified_data_per_row = set_out_of_control_points_to_black_per_row(original_data, out_of_control_points_per_row)
 
 # Call the function to get the modified data for columns
 control_limits_per_column = calculate_control_limits_per_column(original_data)
-out_of_control_points_per_column = find_out_of_control_points(original_data.T, control_limits_per_column)
-modified_data_per_column = set_out_of_control_points_to_black(original_data.T, out_of_control_points_per_column)
+out_of_control_points_per_column = find_out_of_control_points_per_column(original_data, control_limits_per_column)
+modified_data_per_column = set_out_of_control_points_to_black_per_column(original_data, out_of_control_points_per_column)
 
 # Orijinal ve değiştirilmiş veri setlerini plot et
 plt.figure(figsize=(18, 6))
@@ -96,7 +118,7 @@ plt.imshow(modified_data_per_row, cmap='gray', vmin=0, vmax=255)
 plt.title("Değiştirilmiş Veri (Row)")
 
 plt.subplot(1, 3, 3)
-plt.imshow(modified_data_per_column.T, cmap='gray', vmin=0, vmax=255)
+plt.imshow(modified_data_per_column, cmap='gray', vmin=0, vmax=255)
 plt.title("Değiştirilmiş Veri (Column)")
 
 plt.show()
